66e9d6b6c87dfa986759ddbd35c8774c4f84878d
William P. <williampratama837@gmail.com>
2025-12-29 14:44:21 +0700

    update: optimize code for otp section

--- webApp/resources/views/auth/otp.blade.php
+++ webApp/resources/views/auth/otp.blade.php
@@ -27,12 +27,12 @@
         @csrf
         <div class='flex flex-col mt-4 mb-8 otp-container'>
             <div class='flex flex-row justify-center gap-4 items-center'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-1" id="otp-1" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-2" id="otp-2" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-3" id="otp-3" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-4" id="otp-4" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-5" id="otp-5" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
-                <input type="text" maxlength="1" inputmode="numeric" name="otp-6" id="otp-6" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
+                @php ($amount = 6)
+                @php ($idx = 1)
+                @while ($idx <= $amount)
+                    <input type="text" maxlength="1" inputmode="numeric" name="otp-{{ $idx }}" id="otp-{{ $idx }}" autocomplete="off" placeholder="*" class='not-placeholder-shown:bg-white bg-shadedOfGray-20 text-center text-paragraph outline-none border-2 font-bold rounded-md px-1.5 py-0.5 w-10 h-12 border-primary-30 placeholder:text-paragraph focus:border-secondary-happy-100 focus:bg-secondary-happy-20/50 ease-in-out duration-150'>
+                    @php ($idx++)
+                @endwhile
             </div>
             <div class="text-error-moderate h-3.5 pt-0.5 mt-2 text-micro md:text-small text-center">
                 @error('otp')
